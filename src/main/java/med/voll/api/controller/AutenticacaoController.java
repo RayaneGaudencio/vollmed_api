7b118a53b69b018f41ad4f6927a3f956fdedcb3b
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-08-14 12:56:35 -0300

    lib JWT adicionada para gerar token e devolver como resposta da requisição

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -3,6 +3,8 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAutenticacao;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,6 +21,9 @@ public class AutenticacaoController {
     @Autowired // anotação para o spring injetar o parâmetro
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
 
@@ -26,7 +31,7 @@ public class AutenticacaoController {
         var authentication = manager.authenticate(token); // retorna um objeto que representa o usuario autenticado no sistema
         // o próprio spring acha a classe AuthenticationService e dentro do método loadUserByUsername busca através do repository o usuário
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 
 }
