62877f5b607bb74b9b5194436e7d35ce92eef962
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-08-14 14:49:33 -0300

    validação do token recebido

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,14 +14,16 @@ import java.io.IOException;
 @Component // classe é um componente genérico
 public class SecurityFilter extends OncePerRequestFilter {// classe executada uma vez por requisição
 
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
 
-        System.out.println(tokenJWT);
+        var subject =  tokenService.getSubject(tokenJWT);
 
         filterChain.doFilter(request, response);
-
     }
 
     private String recuperarToken(HttpServletRequest request) {
