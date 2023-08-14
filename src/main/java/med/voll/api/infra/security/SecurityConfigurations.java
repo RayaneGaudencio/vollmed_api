d924d033d4d4fe73c49576388273a13017521d28
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-08-14 16:25:12 -0300

    alterada a ordem dos filters para que passe primeiro pelo securityfilter e o usuário possa ser autenticado

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity // para personalizar as configurações de segurança
 public class SecurityConfigurations {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean // anotação para devolver um objeto para o Spring
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -23,7 +28,8 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.POST, "/login").permitAll() // permite que todos os usuários enviem requisições para login
                 .anyRequest().authenticated() // todos os outros deverão ser autenticados
-                .and().build();
+                .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class) // define a ordem dos filtros
+                .build();
     }
 
     // método que ensina ao spring como criar um objeto AuthenticationManager
