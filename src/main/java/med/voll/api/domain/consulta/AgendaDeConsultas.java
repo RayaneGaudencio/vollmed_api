f50e63e2de35d3f7a6e2127643188db5da8be615
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-08-16 12:26:45 -0300

    validando dados que chegam na requisição

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,5 +1,6 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,14 @@ public class AgendaDeConsultas {
     @Autowired
     private PacienteRepository pacienteRepository;
     public void agendar(DadosAgendamentoConsulta dados) {
+        // checagem dos dados
+        if (!pacienteRepository.existsById(dados.idPaciente())) { // o repository possui o método que retorna se existe esse id no banco
+            throw new ValidacaoException("Id do paciente informado não existe");
+        }
+
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoException("Id do paciente informado não existe");
+        }
 
         var paciente = pacienteRepository.findById(dados.idPaciente()).get(); // usando repository pois nos dados vem apenas o id do paciente
         var medico = medicoRepository.findById(dados.idMedico()).get(); // findById retorna um Optional então por isso é necessário o .get()
