f0c41e6136baa259e8e55bfd163953e7eaf7c1d0
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-08-16 14:51:21 -0300

    chamando cada método 'validar' das classes que implementam a interface ValidadorAgendamentoDeConsulta

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,11 +1,14 @@
 package med.voll.api.domain.consulta;
 
 import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.validacoes.ValidadorAgendamentoDeConsulta;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AgendaDeConsultas {
@@ -18,6 +21,10 @@ public class AgendaDeConsultas {
 
     @Autowired
     private PacienteRepository pacienteRepository;
+
+    @Autowired
+    private List<ValidadorAgendamentoDeConsulta> validadores; // Spring automaticamente procura todas as classes que implementam e injeta na lista
+
     public void agendar(DadosAgendamentoConsulta dados) {
         // checagem dos dados
         if (!pacienteRepository.existsById(dados.idPaciente())) { // o repository possui o método que retorna se existe esse id no banco
@@ -27,6 +34,8 @@ public class AgendaDeConsultas {
         if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
             throw new ValidacaoException("Id do médico informado não existe");
         }
+
+        validadores.forEach(v -> v.validar(dados)); // percorre cada validador que implementa a classe ValidadorAgendamentoDeConsulta
 
         var paciente = pacienteRepository.findById(dados.idPaciente()).get(); // usando repository pois nos dados vem apenas o id do paciente
         var medico = escolherMedico(dados);
