becc6694464212fda7d9b67a571d9300860df1ab
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-08-16 14:09:51 -0300

    adicionada classes de validação

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -27,4 +27,13 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
             limit 1
             """) // os dois pontos antes da especialidade quer dizer que é o parâmetro do método, que são exatamente iguais
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
+
+
+    @Query("""
+            select m.ativo
+            from Medico m
+            where
+            m.id = :id
+            """)
+    Boolean findAtivoById(Long id);
 }
